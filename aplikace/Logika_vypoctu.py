ea2a96e2915b04f18b19ba8ad8f87850375a704c
Jana Jedelska <ja.jedelska@gmail.com>
2018-05-12 21:11:47 +0200

    Prvni branch Nezer jako prase app.

--- aplikace/Logika_vypoctu.py
+++ aplikace/Logika_vypoctu.py
@@ -2,15 +2,14 @@ import psycopg2
 import random
 
 def selecty_z_databaze(select):
-	conn = psycopg2.connect(dbname="****", user="****", password= "****", host="da.stderr.cz") #ted se zacnu pripojovat do dataaze
+	conn = psycopg2.connect(dbname="janicka", user="janicka", password= "Tajne heslo, ktere neni na githubu", host="da.stderr.cz") #ted se zacnu pripojovat do dataaze
 	cur1 = conn.cursor() #pomoci cursoru delam to, co chci delat na databazi
-
 	cur1.execute(select) 
 	return cur1.fetchall() 
 
-tabulka_potravin = selecty_z_databaze("SELECT p.nazev, p.kj, mj.nazev FROM potraviny p join merna_jednotka mj on p.id_merna_jednotka = mj.id")
-
-
+tabulka_potravin = selecty_z_databaze("SELECT p.nazev, p.kj, p.id_kategorie, mj.zkratka FROM potraviny p join merna_jednotka mj on p.id_merna_jednotka = mj.id")
+tabulka_potravin_tmp = list(tabulka_potravin)
+#print (tabulka_potravin)
 
 kj_potrebne = 40
 
@@ -35,18 +34,21 @@ def vyber_jidla(KJ_k_snedku):
 
 		if vybrana_potravina_kj < KJ_k_snedku - vybrane_kj:
 			seznam_vybranych_potravin.append(vybrana_potravina)
+			#tabulka_potravin.remove(vybrana_potravina) #toto zajisti, ze kdyz ze zasob snim banan na snidani, tak uz se mi nenabidne i na obed, protoze jsem ho snedla
 			vybrane_kj = vybrane_kj + vybrana_potravina_kj
 			print(vybrane_kj)
-		else:
-			continue
-
+		
 	return (vybrane_kj, seznam_vybranych_potravin)
 
 
 
+tabulka_potravin = filter(lambda x: x[2] not in [13, 40, 67, 68], tabulka_potravin)
+#tabulka_potravin = [x for x in tabulka_potravin if x.id_kategorie != 13]
+#tabulka_potravin.remove(lambda id_kategorie: id_kategorie not in [13,40,67,68])
 SNIDANE = vyber_jidla(kj_potrebne_snidane)
 print(SNIDANE)
 
+tabulka_potravin = list(tabulka_potravin_tmp)
 SVACINA1 = vyber_jidla(kj_potrebne_svacina1)
 print(SVACINA1)
 
