9c02b2564acaffefd312b216a35760686a05d42f
chicquita <38797257+chicquita@users.noreply.github.com>
2018-06-03 00:38:00 +0200

    uprava tabulky

--- forms.py
+++ forms.py
@@ -1,12 +1,22 @@
 #BUDE SLLOUZIT CASEM PRO PRIHLASENI, KOD NIZE JE POUZE HRUBY NASTREL, KTERY ZATIM NEOBSAHUJE VSECHNY POTREBNE STRINGFIELDS
 
-from flask_wtf import Form 
-from wtforms import StringField, PasswordField, SubmitField
+from flask_wtf import FlaskForm 
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, RadioField, IntegerField, SelectField
 from wtforms.validators import DataRequired, Email, Length
 
-class SignupForm(Form):
-  first_name = StringField('First name', validators=[DataRequired("Please enter your first name.")])
-  last_name = StringField('Last name', validators=[DataRequired("Please enter your last name.")])
-  email = StringField('Email', validators=[DataRequired("Please enter your email address."), Email("Please enter your email address.")])
-  password = PasswordField('Password', validators=[DataRequired("Please enter a password."), Length(min=6, message="Passwords must be 6 characters or more.")])
-  submit = SubmitField('Sign up')
+class SignupForm(FlaskForm):
+  first_name = StringField('Jméno', validators=[DataRequired("Zadej jméno.")])
+	last_name = StringField('Příjmení', validators=[DataRequired("Zadej příjmení.")])
+	email = StringField('Email', validators=[DataRequired("Zadej email."), Email("Prosím zadejte emailovou adresu.")])
+	password = PasswordField('Heslo', validators=[DataRequired("Zadej heslo."), Length(min=6, message="Heslo musí obsahovat 6 a více znaků.")])
+	nickname = StringField('Přezdívka', validators=[DataRequired("Zadej přezdívku.")])
+	household = StringField('Domácnost', validators=[DataRequired("Zadej jméno domácnosti.")])
+	sex = RadioField('Pohlaví', choices=[('zena','Žena'), ('muz','Muž')])
+	age = IntegerField('Věk', validators=[DataRequired("Zadej věk.")])
+	weight = IntegerField('Váha (kg)', validators=[DataRequired("Zadej váhu.")])
+	height = IntegerField('Výška (cm)', validators=[DataRequired("Zadej výšku.")])
+	dailykj = IntegerField('Denní příjem kj', validators=[DataRequired("Zadej denní příjem kj.")])
+	makrobilkoviny = IntegerField('Makro bílkoviny (g)', validators=[DataRequired("Zadej makro bílkoviny.")])
+	makrosacharidy = IntegerField('Makro sacharidy (g)', validators=[DataRequired("Zadej makro sacharidy.")])
+	makrotuky = IntegerField('Makro tuky (g)', validators=[DataRequired("Zadej makro tuky.")])
+	submit = SubmitField('Registrovat')
